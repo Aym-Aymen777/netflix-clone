17b5825f52cf9980716a65c249ef2d611cb2bd9c
Aym-Aymen777 <aymenchairi418@gmail.com>
2025-05-09 15:23:24 +0200

    deployement

---
+++ backend/utils/envVars.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve("./.env") });
+
+export const envVars = {
+  PORT: process.env.PORT || 3000,
+  mongoURI: process.env.MONGO_URI ,
+  jwtSecret:process.env.JWT_SECRET,
+  NODE_ENV :process.env.NODE_ENV,
+  TMDB_API_KEY:process.env.TMDB_API_KEY
+}
