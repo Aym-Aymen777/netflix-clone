17d7b2ef6c934933f40d8873a981d5bae1c79443
Aym-Aymen777 <aymenchairi418@gmail.com>
2025-05-30 17:23:07 +0200

    deploy

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,9 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('token') || ''}`,
-  },
+  baseURL: import.meta.env.VITE_API_URL+"/api/v1", // تأكد أنها تشير إلى السيرفر الصحيح
+  withCredentials: true, // إذا كنت تستخدم الـ cookie
 });
+
+// ✅ إضافة التوكن تلقائيًا في كل الطلبات
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("jwt-netflix"); // هنا اسم التوكن
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // إرسال التوكن
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api;
