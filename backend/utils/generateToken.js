287a298fe8fe47513c69c92c3809f4fb9dd650ec
Aym-Aymen777 <aymenchairi418@gmail.com>
2025-05-30 23:34:36 +0200

    deploy

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookies = (userId, res) => {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks, make it not be accessed by JS
 		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-		secure: envVars.NODE_ENV === "development",
+		secure: true,	 // set to true if using HTTPS, false for HTTP
 	});
 
 	return token;
