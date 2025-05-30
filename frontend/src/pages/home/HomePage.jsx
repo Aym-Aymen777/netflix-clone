1b1e8cb36bcd515a3f327331659f74f37eca391c
Aym-Aymen777 <aymenchairi418@gmail.com>
2025-05-31 00:19:24 +0200

    deploy

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   const {user} = useAuthStore()
   return (
     <div>
-      {!user ? <HomeScreen/>:<AuthScreen/>}
+      {user ? <HomeScreen/>:<AuthScreen/>}
     </div>
   )
 }
